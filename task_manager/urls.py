f64beec62eae8a311f2984365b42c841c3169894
aasakib <johnsakibcharls@gmail.com>
2023-11-28 12:01:09 +0600

    final

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf.urls import handler404
+handler404 = 'task.views.error_404'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("task.urls")),
